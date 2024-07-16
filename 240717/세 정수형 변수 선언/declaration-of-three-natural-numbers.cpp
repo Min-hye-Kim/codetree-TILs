0ee84edcefb351f8afd09c64c315da730d4a5a79
Min-hye-Kim <kimilu0108@gmail.com>
2024-07-17 03:44:59 +0900

    Title: 세 정수형 변수 선언, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240717/세 정수형 변수 선언/declaration-of-three-natural-numbers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+	int a = 7;
+
+	int b = 23;
+
+	int c = 30;
+
+	cout << a << " + " << b << " = " << c;
+
+	return 0;
+
+}
