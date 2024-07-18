e9365a76c89e9c85e5b0cf758d7c6812b6d8193f
Min-hye-Kim <kimilu0108@gmail.com>
2024-07-18 12:57:01 +0900

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240718/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int zero, first, second;
+    char e;
+    cin>>zero>>e>>first>>e>>second;
+
+    cout<<"010-"<<second<<e<<first;
+
+    return 0;
+}
